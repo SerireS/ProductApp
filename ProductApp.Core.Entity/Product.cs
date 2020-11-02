4cc6a7c25ab97a0dc0ca3afa1311028d429b4180
SerireS <55481182+SerireS@users.noreply.github.com>
2020-11-02 11:42:39 +0100

    Add files via upload

---
+++ ProductApp.Core.Entity/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProductApp.Core.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Color { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ ProductApp.Infrastructure.SQLLite.Data/DBInitializer.cs
@@ -0,0 +1,25 @@
+﻿using ProductApp.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductApp.Infrastructure.SQLLite.Data
+{
+    public class DBInitializer: IDBInitializer
+    {
+        public void SeedDB(ProductAppLiteContext ctx) 
+        {
+            ctx.Database.EnsureDeleted();
+            ctx.Database.EnsureCreated();
+
+            ctx.Products.Add(new Product()
+            {
+                Name = "James Bond",
+                Price = 007,
+                Color = "Red",
+                Type = "Spy",
+            });
+            ctx.SaveChanges();
+        }
+    }
+}
